9ac608709e6471892ac4d72330106d92f16c8d39
mushahiroyuki <hmusha@gmail.com>
2017-02-02 18:01:19 +0900

    無限ループの例 5-4-4を修正

--- example/ch05/ex05-04-4/main.js
+++ example/ch05/ex05-04-4/main.js
@@ -5,6 +5,6 @@ while(true) {
   console.log(x);
   if(x === 0.3) break;
 
-	if (counter++ > 1000) break; // 無限ループになってしまって困る方が出るので、追加しました。（訳者）
+	if (counter++ > 100) break; // 無限ループになってしまって困る方が出るので、追加しました。（訳者）
 }
 console.log(`${x}で停止。`);
