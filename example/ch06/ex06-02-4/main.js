53b40ea1354b0c5c6a8d00fb0c391010eeb2879d
musha marlin <musha@marlin.local>
2017-07-13 11:02:37 +0900

    7/13/2017 訂正。

--- example/ch06/ex06-02-4/main.js
+++ example/ch06/ex06-02-4/main.js
@@ -4,7 +4,7 @@ function getGreeting() {
 
 // #@@range_begin(list1)
 const arr = [1, 2, 3];
-arr[1] = getGreeting; // arr は[1, function getGreeting(), 2]になる
+arr[1] = getGreeting; // arr は[1, function getGreeting(), 3]になる
 const message = arr[1](); 
 console.log(message); // Hello world!
 // #@@range_end(list1)
--- example/ch20/ex20-10-1/server1.js
+++ example/ch20/ex20-10-1/server1.js
@@ -5,7 +5,7 @@ const server = http.createServer(function(req, res) {
   res.end('Hello world!');
 });
 
-const port = 8081;
+const port = 8080;
 server.listen(port, function() {
   // サーバーが開始されたことを知らせるコールバック関数を渡すことができる
   console.log(`サーバーをポート${port}で開始`);
