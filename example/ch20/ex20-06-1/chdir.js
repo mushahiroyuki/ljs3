26d26bbf8e5778fabb6bace99df4cf641cf4bd51
musha marlin <hmusha@gmail.com>
2019-02-10 04:35:52 +0900

    タイポ修正

--- example/ch20/ex20-06-1/chdir.js
+++ example/ch20/ex20-06-1/chdir.js
@@ -1,3 +1,3 @@
 console.log(`現在のディレクトリ：${process.cwd()}`);
-process.chdir(__dirname); // プログラムが起動されてディレクトリへ移動
+process.chdir(__dirname); // プログラムが起動されたディレクトリへ移動
 console.log(`新しいCWDディレクトリ: ${process.cwd()}`);
