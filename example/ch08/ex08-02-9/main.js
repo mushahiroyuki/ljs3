c8a572183efc18e832606da212cb7842d313a73b
musha hiroyuki <hmusha@gmail.com>
2021-11-29 20:10:39 +0900

    correction -- example/ch08/ex08-02-9/main.js

--- example/ch08/ex08-02-9/main.js
+++ example/ch08/ex08-02-9/main.js
@@ -1,10 +1,23 @@
 let arr = [{ name: "Suzanne" }, { name: "Jim" },
 	     { name: "Trevor" }, { name: "Amanda" }];
 console.log(arr);
-arr.sort((a, b) => a.name > b.name);  // nameでソート
+arr.sort((a, b) => a.name > b.name ? 1 : (a.name === b.name ? 0 : -1)); // nameでソート
+/* たとえば次でもOKです　
+arr.sort(function (a, b) {
+  if (a.name > b.name) {
+    return 1;
+  }
+  else if (a.name === b.name) {
+    return 0;
+  }
+  else {
+    return -1;
+  }
+})
+*/
 console.log("------");
 console.log(arr);
-arr.sort((a, b) => a.name[1] < b.name[1]); // nameの2文字目で逆順にソート
+arr.sort((a, b) => a.name[1] < b.name[1] ? 1 : (a.name[1] === b.name[1] ? 0 : -1)); // nameの2文字目で逆順にソート
 console.log("------");
 console.log(arr);
 
